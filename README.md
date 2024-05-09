7fbed04084f83ef7789e8bcc0f8c766c14837475
Rayhan9966 <rayhancse162gmail.com>
2024-05-09 22:14:53 +0400

    mongodb connected successfully

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ Assignment_ID: assignment_category_0010
 
 
 
-##client-side code GitHub repository: https://github.com/Porgramming-Hero-web-course/b9a11-client-side-Rayhan9966
+*client-side code GitHub repository: https://github.com/Porgramming-Hero-web-course/b9a11-client-side-Rayhan9966
 
 
-##server-side code GitHub repository: https://github.com/Porgramming-Hero-web-course/b9a11-server-side-Rayhan9966
+*server-side code GitHub repository: https://github.com/Porgramming-Hero-web-course/b9a11-server-side-Rayhan9966
 
 
 
-Live link:
+**Live link:
 
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port =process.env.PORT|| 5000;
 
+
+console.log(process.env.DB_PASS);
 //midleware
 
 app.use(cors());
@@ -13,7 +16,7 @@ app.use(express.json());
 //mongodb
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.ptxksnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ptxksnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
